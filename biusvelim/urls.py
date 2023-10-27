a1c05e37bc5879eec934a07c91c96078f855c165
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2023-10-27 15:03:07 -0500

    intento de perfiles

--- biusvelim/urls.py
+++ biusvelim/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path 
-from .views import Home
-
+from .views import Home, PerfilList
 app_name = 'biusvelim'
 
 urlpatterns = [
-    path('', Home.as_view(), name='Home')
+    path('', Home.as_view(), name='Home'),
+    path('profiles', PerfilList.as_view(), name='perfil_list')
 ]
--- biusvelim/views.py
+++ biusvelim/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 
 class Home(View):
     def get(self, request, *args, **kwargs):
             return render(request, 'index.html')
+    
+
+method_decorator(login_required, name='dispatch')
+class PerfilList(View):
+      def get(self, request, *args, **kwargs):
+            perfil = request.user.perfiles.all()
+
+            context = {
+                  'perfil': perfil
+            }
+
+            return render(request, 'PerfilLista.html', context)
