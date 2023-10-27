e13a64aec1d5cef4b391263798561e8ee39d6993
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2023-10-27 14:03:46 -0500

    empezando login

--- biusvelim/admin.py
+++ biusvelim/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from biusvelim.models import Pelicula, Video, Perfil, CustomUser
 # Register your models here.
+admin.site.register(Pelicula)
+admin.site.register(Video)
+admin.site.register(Perfil)
+admin.site.register(CustomUser)
--- npi/settings.py
+++ npi/settings.py
@@ -39,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'biusvelim',
 
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
 
 ]
 
@@ -50,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'npi.urls'
@@ -57,7 +62,7 @@ ROOT_URLCONF = 'npi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,14 +128,29 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 AUTH_USER_MODEL = 'biusvelim.CustomUser'
+AUTHENTICATION_BACKENDS = [
+    
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by email
+    'allauth.account.auth_backends.AuthenticationBackend',
+    
+]
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+LOGIN_URL = 'login'
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+ACCOUNT_USERNAME_REQUIRED = False
 #LOGIN_REDIRECT_URL = ''
-#LOGIN_URL = 'login'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
