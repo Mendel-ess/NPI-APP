c9febaa165840b13eca590c2caf0b25ba16479ae
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2023-10-27 09:53:11 -0500

    some changes

--- npi/settings.py
+++ npi/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'peliculas'
+    'peliculas',
+    'Homepage',
+
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'npi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- peliculas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PeliculaList, PeliculaDetail
-
-urlpatterns = [
-        path('', PeliculaList.as_view(), name='pelicula_list'),
-        path('<int:pk>', PeliculaDetail.as_view(), name='pelicula_detail')
-    ]
