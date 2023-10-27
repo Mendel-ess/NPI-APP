59902f77a3339325e97f778c20d3d08a6a942154
Jhonnatan Mendez <jhonnatandavid34mc@gmail.com>
2023-10-27 14:32:35 -0500

    login, signup templates

--- biusvelim/urls.py
+++ biusvelim/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
 from .views import Home
 
+app_name = 'biusvelim'
+
 urlpatterns = [
-    path('', Home.as_view(), name='home')
+    path('', Home.as_view(), name='Home')
 ]
